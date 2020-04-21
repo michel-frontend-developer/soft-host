fc154b767f9e91fb39e61ade963783f1b8c2bc15
michel-desenvolvedor-web <michel.desenvolvedorweb@gmail.com>
2020-04-20 21:52:48 -0300

    mhost finalizado com sucesso

---
+++ js/toggleMenu.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//  menu toggle
+const navbarToggle = document.querySelector('.btn-toggle');
+const navbarMenu = document.querySelector('.header-items');
+const navbarLinks = document.querySelectorAll('.header-items .header-link');
+
+navbarToggle.addEventListener('click', function(){
+    navbarMenu.classList.toggle('open');
+}, false);
+
+navbarLinks.forEach(function(element){
+    element.addEventListener('click', function(){
+        if(navbarMenu.classList.contains('open')){
+            navbarToggle.click();
+        }
+    }, false);
+});
